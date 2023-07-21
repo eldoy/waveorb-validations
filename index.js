98436b6d3292b3ea7387c067868feb3e90065626
Vidar Eldøy <vidar@eldoy.com>
2023-07-21 07:27:45 +0200

    Remove types

--- index.js
+++ index.js
@@ -5,15 +5,12 @@ var owner = require('./lib/owner.js')
 module.exports = function validations($) {
   return {
     unique: {
-      type: 'string|object',
       fn: unique($)
     },
     exist: {
-      type: 'string',
       fn: exist($)
     },
     owner: {
-      type: 'string',
       fn: owner($)
     }
   }
